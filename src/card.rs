14b856d784e748d1738df5f656608293ca1cf397
LapinoLapidus <LapinoLapidus@users.noreply.github.com>
2022-05-10 23:58:16 +0200

    working please don't mess up

---
+++ src/card.rs
@@ -0,0 +1,28 @@
+pub mod card {
+    use std::fmt::{Debug, Formatter};
+    use ansi_term::Colour::{Blue, Yellow, Green, Cyan, RGB, White, Red};
+
+    pub struct Card {
+        pub game_name: String,
+        pub game_id: String,
+        pub name: String,
+        pub stock: String,
+        pub worth: String,
+        pub price: String,
+    }
+
+    impl Debug for Card {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{} - {}\n{} - {}\n{} - {}\n{} - {}\n",
+                   Yellow.bold().paint(&self.game_name),
+                   Blue.bold().paint(&self.name),
+                   RGB(255, 165, 0).bold().paint("Stock"),
+                   White.bold().paint(&self.stock),
+                   RGB(230, 230, 250).bold().paint("Price"),
+                   White.paint(&self.price),
+                   RGB(131, 67, 32).bold().paint("Worth"),
+                   White.paint(&self.worth)
+                    )
+        }
+    }
+}
